49e01555223e869cbbeeab46ab843a7e002d5984
edmarsht <edtoulet@gmail..com>
2021-07-22 17:07:20 +0200

    login auth created

--- src/spotify.js
+++ src/spotify.js
@@ -10,3 +10,7 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
   ];
+
+  
+
+  export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
