1f139efcf43a9c926ab3c5a7af8780f815337e14
edmarsht <edtoulet@gmail..com>
2021-07-27 11:57:57 +0200

    add new icons on body.js

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,10 @@
-import { Description } from '@material-ui/icons';
+import { Description, PlayCircleFilled } from '@material-ui/icons';
 import React from 'react';
 import "./Body.css";
 import Header from "./Header";
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { useDataLayerValue } from './DataLayer';
 
 function Body( { spotify }) {
@@ -18,6 +21,14 @@ function Body( { spotify }) {
                     <h2>Discover Weekly</h2>
                     <p>Your weekly mixtape of fresh music. Enjoy and deep cuts picked for you. Updates every Monday.</p>
                     {/* <p>{discover_weekly?.description}</p> */}
+                </div>
+            </div>
+
+            <div className="body__songs">
+                <div className="body__icon">
+                    <PlayCircleFilledIcon />
+                    <FavoriteIcon />
+                    <MoreHorizIcon />
                 </div>
             </div>
         </div>
--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,8 @@ export const initialState = {
     playing: false, 
     item: null, 
     // remove after finished developing"
-    // token: 'BQD7DOIBMME2pz9zPMNSCuBC1Y02qO0CDvbCTi1TherAzJFHEBlmxK8jH4Jk1DiS5LGOB4bdl8VuzTqhtmdNPWnKH1kY9zpzQkU2P-91eHUrS8Yc3EdsBjbrmsKYGNp0JKhhwBsOtgKKXwEIWDV0URH43iVtjSkN9akVuP5P659IaIqrHd1w'
-    // ,
+    token: 'BQD7DOIBMME2pz9zPMNSCuBC1Y02qO0CDvbCTi1TherAzJFHEBlmxK8jH4Jk1DiS5LGOB4bdl8VuzTqhtmdNPWnKH1kY9zpzQkU2P-91eHUrS8Yc3EdsBjbrmsKYGNp0JKhhwBsOtgKKXwEIWDV0URH43iVtjSkN9akVuP5P659IaIqrHd1w'
+    ,
 }; 
 
 const reducer = (state, action) => {
@@ -35,7 +35,7 @@ const reducer = (state, action) => {
           ...state,
           discover_weekly: action.discover_weekly,
         };
-        
+
     default: 
         return state;
   }
